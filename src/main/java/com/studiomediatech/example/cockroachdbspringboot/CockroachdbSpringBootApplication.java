bfff5fcc3472cc82ed97af4b402fcc90ff97083a
Olle Törnström <toernstroem@synyx.de>
2018-07-06 12:57:57 +0200

    Initial commit

---
+++ src/main/java/com/studiomediatech/example/cockroachdbspringboot/CockroachdbSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.studiomediatech.example.cockroachdbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockroachdbSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockroachdbSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/studiomediatech/example/cockroachdbspringboot/CockroachdbSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.studiomediatech.example.cockroachdbspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CockroachdbSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
