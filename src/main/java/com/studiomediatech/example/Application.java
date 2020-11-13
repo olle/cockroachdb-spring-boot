7da4a7009af8f7d185b5b71288e295788a03fe58
Olle Törnström <toernstroem@synyx.de>
2020-11-13 23:55:12 +0100

    Barely got 2 data sources working... it seems

--- src/main/java/com/studiomediatech/example/Application.java
+++ src/main/java/com/studiomediatech/example/Application.java
@@ -1,12 +1,69 @@
 package com.studiomediatech.example;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Configuration
+    public static class Config {
+
+        @Bean
+        @ConditionalOnMissingBean
+        JdbcTemplate jdbcTemplate(HikariDataSource clusterDataSource) {
+
+            return new JdbcTemplate(clusterDataSource);
+        }
+
+
+        @Bean
+        @Primary
+        @ConfigurationProperties("app.datasource.cluster")
+        DataSourceProperties clusterDataSourceProps() {
+
+            return new DataSourceProperties();
+        }
+
+
+        @Bean
+        @ConfigurationProperties("app.datasource.cluster.configuration")
+        HikariDataSource clusterDataSource(DataSourceProperties clusterDataSourceProps) {
+
+            return clusterDataSourceProps.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+        }
+
+
+        @Bean
+        @ConfigurationProperties("app.datasource.archive")
+        DataSourceProperties archiveDataSourceProps() {
+
+            return new DataSourceProperties();
+        }
+
+
+        @Bean
+        @ConfigurationProperties("app.datasource.archive.configuration")
+        HikariDataSource archiveDataSource(DataSourceProperties archiveDataSourceProps) {
+
+            return archiveDataSourceProps.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+        }
+    }
 }
